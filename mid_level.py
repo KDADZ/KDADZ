8d9cd11ae6dfd40bf2533a591b6b741412d4647d
Armando Bugarin <armando.bugarin24@gmail.com>
2024-02-06 17:19:08 -0800

    edit mid_level.py; stickman move functionality

--- mid_level.py
+++ mid_level.py
@@ -3,6 +3,8 @@ import sys
 
 pygame.init()
 
+portal_categories = ['a) history', 'b) video games', 'c) sports', 'd) food']
+
 screen_width, screen_height = 1000, 800
 screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption('Trivia Trek')
@@ -18,19 +20,17 @@ portal_image = pygame.transform.scale(portal_image, (100, 100))
 shop_portal_image = pygame.image.load('assets/img/portal2.png').convert_alpha()
 shop_portal_image = pygame.transform.scale(shop_portal_image, (50, 50))
 
-# Load each stick figure image
-stick_figure_images = [
-    pygame.image.load('assets/img/stickman1.png').convert_alpha(),
-    pygame.image.load('assets/img/stickman2.png').convert_alpha(),
-    pygame.image.load('assets/img/stickman3.png').convert_alpha(),
-    pygame.image.load('assets/img/stickman4.png').convert_alpha(),
-]
+# Load stick figure image
+stick_figure_image = pygame.image.load('assets/img/stickman.png').convert_alpha()
+stick_figure_image = pygame.transform.scale(stick_figure_image, (50, 100))
 
-# Ensure all stick figures are scaled to fit above the portals
-for i in range(len(stick_figure_images)):
-    stick_figure_images[i] = pygame.transform.scale(stick_figure_images[i], (50, 100))
+# Ensure stick figure is scaled to fit above the portals
+stick_figure_image = pygame.transform.scale(stick_figure_image, (50, 100))
 
-num_portals = len(stick_figure_images)  # Now you have 4 stick figures and 4 portals
+# set initial stickman position
+stickman_position = (screen_width - stick_figure_image.get_size()[0]) // 2
+
+num_portals = len(portal_categories)  # 1 stick figure and 4 portals
 
 # Colors
 white = (255, 255, 255)
@@ -39,12 +39,50 @@ black = (0, 0, 0)
 # Font
 font = pygame.font.Font(None, 36)
 
+stickman_speed = 1
+fast_speed = 1
+is_left_held = False
+is_right_held = False
+
+def handle_events():
+    global stickman_position, is_left_held, is_right_held
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                is_left_held = True
+            elif event.key == pygame.K_RIGHT:
+                is_right_held = True
+        elif event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT:
+                is_left_held = False
+            elif event.key == pygame.K_RIGHT:
+                is_right_held = False
+
+def check_collisions(portal_x, portal_size, stickman_x, stickman_y, stickman_size):
+    return (
+        stickman_x < portal_x + portal_size and
+        stickman_x + stickman_size > portal_x and
+        stickman_y < screen_height - portal_size and
+        stickman_y + stickman_size > (screen_height - portal_size) // 2
+    )
+
 def game_scene():
+    global stickman_position
     while True:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                sys.exit()
+        handle_events()
+
+        # adjust speed of stickman
+        current_speed = fast_speed if (is_left_held or is_right_held) else stickman_speed
+
+        # update stickman position
+        if is_left_held:
+            stickman_position -= current_speed
+        elif is_right_held:
+            stickman_position += current_speed
 
         # Draw the background
         screen.blit(background_image, (0, 0))
@@ -52,20 +90,29 @@ def game_scene():
         portal_size = portal_image.get_size()[0]
         portal_spacing = (screen_width - (num_portals * portal_size)) // (num_portals + 1)
         portal_start_x = portal_spacing
-        portal_y = (screen_height - portal_size) // 2
+        # portal_y = (screen_height - portal_size) // 2
 
-        # Draw portals and stick figures
+        # Draw portals and stick figure
         for i in range(num_portals):
             portal_x = portal_start_x + i * (portal_size + portal_spacing)
+            portal_y = (screen_height - portal_size) // 2
             screen.blit(portal_image, (portal_x, portal_y))
 
-            # Get the current stick figure image
-            stick_figure_image = stick_figure_images[i]
-            stick_figure_x = portal_x + (portal_size - stick_figure_image.get_size()[0]) // 2
-            stick_figure_y = portal_y - stick_figure_image.get_size()[1] - 10  # 10 pixels above the portal
-
-            # Draw the stick figure above the portal
+            # draw stick figure above portals
+            stick_figure_x = stickman_position
+            stick_figure_y = portal_y - stick_figure_image.get_size()[1] - 10 # 10 pixels above the portal
             screen.blit(stick_figure_image, (stick_figure_x, stick_figure_y))
+
+            if check_collisions(portal_x, portal_size, stick_figure_x, stick_figure_y, stick_figure_image.get_size()[1]):
+                 print(f"Stickman collided with portal {portal_categories[i]}")
+
+            # render and display portal categories
+            category_font_size = 30
+            category_font = pygame.font.Font(None, category_font_size)
+            category_text = category_font.render(portal_categories[i], True, white)
+            category_x = portal_x + (portal_size - category_text.get_size()[0]) // 2
+            category_y = portal_y + portal_size + 5 # 5 pixels below the portal
+            screen.blit(category_text, (category_x, category_y))
 
         # Draw shop portal
         shop_portal_x = screen_width - shop_portal_image.get_size()[0] - 20
@@ -74,7 +121,7 @@ def game_scene():
 
         # Footer text
         level_difficulty = "Easy/Medium/Hard"
-        footer_text = font.render(f"Level Difficulty: {level_difficulty}", True, black)
+        footer_text = font.render(f"Level Difficulty: {level_difficulty}", True, white)
         screen.blit(footer_text, (10, screen_height - 40))
 
         pygame.display.flip()
