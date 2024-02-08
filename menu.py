6dd2e2bf06920da7a168a16a422aa179f68d8f37
HighMid <xstar671@gmail.com>
2024-02-08 12:08:02 -0500

    Updated stuff

--- menu.py
+++ menu.py
@@ -11,7 +11,7 @@ class Menu:
         self.screen = game.screen
         self.screen_width, self.screen_height = self.screen.get_size()
         self.current_state = 'main_menu'
-        pygame.mixer.music.load('assets\Music\main_menu.mp3')
+        pygame.mixer.music.load('assets/Music/main_menu.mp3')
         pygame.mixer.music.play(-1)
         
         self.background_image = pygame.image.load('assets/img/background.jpg').convert_alpha()
@@ -27,6 +27,18 @@ class Menu:
         self.back_button_rect = pygame.Rect(50, 50, 100, 50)
         self.restart_button_rect = pygame.Rect(self.screen_width // 2 - 100, 400, 200, 50)
         self.quit_button_rect = pygame.Rect(self.screen_width // 2 - 100, 475, 200, 50)
+        
+        # Attributes for the volume slider
+        # self.volume_slider_rect = pygame.Rect(self.screen_width // 2 - 100, 350, 200, 20)
+        # self.volume_handle_rect = pygame.Rect(self.volume_slider_rect.centerx - 10, self.volume_slider_rect.y - 5, 20, 30)
+        self.volume_slider_rect = pygame.Rect(self.screen_width // 2 - 150, 450, 300, 30)
+        self.volume_handle_rect = pygame.Rect(self.volume_slider_rect.centerx - 15, self.volume_slider_rect.y - 10, 30, 50)
+        self.volume = pygame.mixer.music.get_volume()
+        self.dragging_volume_handle = False
+        self.update_volume_handle_pos()
+
+    def update_volume_handle_pos(self):
+        self.volume_handle_rect.centerx = self.volume_slider_rect.x + self.volume * self.volume_slider_rect.width
 
     def start_game(self):
         self.game.transition_state(GameState.MID_LEVEL)
@@ -40,28 +52,28 @@ class Menu:
 
     def handle_events(self, events):
         mouse_pos = pygame.mouse.get_pos()
-        mouse_clicked = False
         
         for event in events:
             if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
-                mouse_clicked = True  # Mouse was clicked this frame
-
-        if self.current_state == 'main_menu':
-            if mouse_clicked:
-                for _, button_data in self.buttons.items():
-                    if button_data["rect"].collidepoint(mouse_pos):
-                        button_data["action"]()
-        elif self.current_state == 'options_menu':
-            if mouse_clicked:
-                if self.restart_button_rect.collidepoint(mouse_pos):
-                    # Handle restart button click
+                if self.current_state == 'options_menu' and self.volume_handle_rect.collidepoint(mouse_pos):
+                    self.dragging_volume_handle = True
+                elif self.back_button_rect.collidepoint(mouse_pos):
                     self.current_state = 'main_menu'
                 elif self.quit_button_rect.collidepoint(mouse_pos):
-                    # Handle quit button click
                     self.quit_game()
-                elif self.back_button_rect.collidepoint(mouse_pos):
-                    # Handle back button click
-                    self.current_state = 'main_menu'
+                else:
+                    for _, button_data in self.buttons.items():
+                        if button_data["rect"].collidepoint(mouse_pos):
+                            button_data["action"]()
+
+            elif event.type == pygame.MOUSEBUTTONUP and event.button == 1:
+                self.dragging_volume_handle = False
+
+            elif event.type == pygame.MOUSEMOTION and self.dragging_volume_handle:
+                new_x = min(max(event.pos[0], self.volume_slider_rect.left), self.volume_slider_rect.right)
+                self.volume_handle_rect.centerx = new_x
+                self.volume = (new_x - self.volume_slider_rect.x) / self.volume_slider_rect.width
+                pygame.mixer.music.set_volume(self.volume)
 
     def draw(self, mouse_pos):
         self.screen.blit(self.background_image, (0, 0))
@@ -78,21 +90,29 @@ class Menu:
 
     def draw_options_menu(self, mouse_pos):
         self.draw_text("Options Menu", self.title_font, self.black, self.screen_width // 2, 60)
-        # Other options we think of
-        self.draw_button("Restart", self.restart_button_rect, self.restart_button_rect.collidepoint(mouse_pos))
-        self.draw_button("Quit", self.quit_button_rect, self.quit_button_rect.collidepoint(mouse_pos))
-        self.draw_back_arrow(mouse_pos)
+        # Draw volume slider and label
+        pygame.draw.rect(self.screen, self.white, self.volume_slider_rect)
+        pygame.draw.rect(self.screen, self.blue if self.volume_handle_rect.collidepoint(mouse_pos) else self.black, self.volume_handle_rect)
+        self.draw_volume_label()
 
-    def draw_button(self, text, rect, active):
-        text_color = self.blue if active else self.black
-        pygame.draw.rect(self.screen, text_color, rect, 2)
-        self.draw_text(text, self.font, text_color, rect.centerx, rect.centery)
+        # Draw other options buttons
+        self.draw_button("Back", self.back_button_rect, self.back_button_rect.collidepoint(mouse_pos))
+
+    def draw_button(self, text, rect, is_hovering):
+        color = self.blue if is_hovering else self.black
+        pygame.draw.rect(self.screen, color, rect)  # Solid fill
+        self.draw_text(text, self.font, self.white, rect.centerx, rect.centery)
 
     def draw_text(self, text, font, color, x, y):
         text_obj = font.render(text, True, color)
         text_rect = text_obj.get_rect(center=(x, y))
         self.screen.blit(text_obj, text_rect)
 
-    def draw_back_arrow(self, mouse_pos):
-        pygame.draw.rect(self.screen, self.black, self.back_button_rect, 2)
-        self.draw_text("Back", self.font, self.black, self.back_button_rect.centerx, self.back_button_rect.centery)
+    def draw_volume_label(self):
+        volume_label_text = "Volume"
+        volume_label_color = self.black
+        volume_label_font = pygame.font.Font(None, 45)  # Increased font size
+        volume_label_surface = volume_label_font.render(volume_label_text, True, volume_label_color)
+        volume_label_x = self.volume_slider_rect.x - volume_label_surface.get_width() - 20  # Adjust position if needed
+        volume_label_y = self.volume_slider_rect.y - volume_label_surface.get_height() // 100  # Adjust for new font size
+        self.screen.blit(volume_label_surface, (volume_label_x, volume_label_y))
